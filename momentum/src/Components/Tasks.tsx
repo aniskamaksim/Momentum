135c54c6fe09e5a69b36dc8e7a551f5dde2ed967
Maksim Aniska <aniskamaksim@gmail.com>
2023-12-01 15:58:58 +0300

    feat: fix edit title logic, make localstored items of weather, tasks and name

--- momentum/src/Components/Tasks.tsx
+++ momentum/src/Components/Tasks.tsx
@@ -1,9 +1,10 @@
-import React, {ChangeEvent, useState, KeyboardEvent, useEffect} from 'react';
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 import {TasksType} from "../App";
 import styled from "styled-components";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Checkbox from '@mui/material/Checkbox';
+import {TextField} from "@mui/material";
 
 type TasksPropsType = {
     tasks: TasksType,
@@ -16,7 +17,6 @@ export const Tasks: React.FC<TasksPropsType> = (
 ) => {
     const oldTitle = tasks.taskTitle
     const [title, setTitle] = useState<string>(oldTitle)
-        console.log(title)
     const [editMode, setEditMode] = useState<boolean>(false)
     const incorrectTitle = title.trim().length === 0;
 
@@ -28,64 +28,70 @@ export const Tasks: React.FC<TasksPropsType> = (
         deleteTask(tasks.taskId)
     }
     const changeModeHandler = () => {
-        setEditMode(!editMode)
+        if (!incorrectTitle) {
+            setEditMode(!editMode)
+        } else {
+            setTitle(oldTitle);
+            setEditMode(!editMode);
+        }
     }
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        const newTitle = event.currentTarget.value
-        console.log(title)
-        console.log(newTitle)
+        const newTitle = event.currentTarget.value;
         setTitle(newTitle);
-        console.log(title)
     }
-    const onClickHandler = (title: string) => {
-            editTaskTitle(tasks.taskId, title)
-            console.log(title)
+    const editTaskTitleHandler = (title: string) => {
+        editTaskTitle(tasks.taskId, title);
     }
     const onKeyDownHandler = (event: KeyboardEvent) => {
         if (event.key === "Enter") {
-            onClickHandler(title);
+            editTaskTitleHandler(title);
             changeModeHandler();
         }
     }
 //TODO: tasklist from/to localStorage put here!
 //TODO: make editable span one for all
-return (
-    <TasksList key={tasks.taskId}>
-        <TaskRow>
+    return (
+        <TasksList key={tasks.taskId}>
+            <TaskRow>
             <span onDoubleClick={changeModeHandler}
                   onBlur={changeModeHandler}>{
-                editMode ? <input type="text"
-                                  value={title}
-                                  autoFocus={true}
-                                  onChange={onChangeHandler}
-                                  // onBlur={onChangeHandler}
-                                  onKeyDown={onKeyDownHandler}
+                editMode ? <TextField
+                    type="text"
+                    variant={'standard'}
+                    value={title}
+                    autoFocus={true}
+                    onChange={onChangeHandler}
+                    inputProps={inputPropsStyle}
+                    // onBlur={onChangeHandler}
+                    onKeyDown={onKeyDownHandler}
                 /> : tasks.taskStatus ?
                     <span style={{textDecoration: "line-through"}}>{title}</span> :
                     <span>{title}</span>
             }
             </span>
-        </TaskRow>
-        <TaskButtons>
-            <Checkbox
-                checked={tasks.taskStatus}
-                onChange={onChangeStatusHandler}
-                inputProps={{'aria-label': 'controlled'}}
-            />
-            <IconButton aria-label="delete" size="small" onClick={deleteTaskHandler}>
-                <DeleteIcon fontSize={"medium"} color={"primary"}/>
-            </IconButton>
-        </TaskButtons>
-    </TasksList>
-)
-}
-;
+            </TaskRow>
+            <TaskButtons>
+                <Checkbox
+                    checked={tasks.taskStatus}
+                    onChange={onChangeStatusHandler}
+                    inputProps={{'aria-label': 'controlled'}}
+                />
+                <IconButton aria-label="delete" size="small" onClick={deleteTaskHandler}>
+                    <DeleteIcon fontSize={"medium"} color={"primary"}/>
+                </IconButton>
+            </TaskButtons>
+        </TasksList>
+    )
+};
+const inputPropsStyle = {
+    style: {padding: "0.3rem", width: "15rem", color: "white", fontSize: "1.4rem", fontWeight: "300"}
+};
 const TasksList = styled.div`
   display: flex;
   flex-direction: row;
   width: 50rem;
   color: #F0F0F0;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   justify-content: space-between;
   align-items: center;
   gap: 0.5rem;
